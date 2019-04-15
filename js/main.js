6599361e4485a736152f681cd7ae999275645fdf
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-04-15 14:04:46 -0400

    some changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+//Sticky Menu background
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 150) {
+        document.querySelector('#nav-bar').style.backgroundColor = "#41aaa8";
+        document.querySelector('.btn-green').style.backgroundColor = "#b5525c";
+        document.querySelector('.btn-green').style.color = "white";
+    } else {
+        document.querySelector('#nav-bar').style.backgroundColor = "transparent";
+        document.querySelector('.btn-green').style.backgroundColor = "#41aaa8";
 
-navBarToggle.addEventListener("click", function () {
-    mainNav.classList.toggle("active");
+    }
 });
